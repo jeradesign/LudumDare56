3a079dcec9f6aba1a11fc7ef6565700536f3c69f
John Brewer <jbrewer@jera.com>
2024-12-02 20:33:07 -0800

    Fix scoring; make ball orange

--- Assets/Scripts/TargetHandler.cs
+++ Assets/Scripts/TargetHandler.cs
@@ -18,7 +18,36 @@ public class TargetHandler : MonoBehaviour
     }
     private void OnCollisionEnter(Collision other)
     {
+        if (other.contacts.Length == 0)
+        {
+            return;
+        }
+
+        ContactPoint collisionPoint = other.contacts[0];
+        Vector2 my2dPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 collision2dPosition = new Vector2(collisionPoint.point.x, collisionPoint.point.y);
+        float distance = Vector2.Distance(my2dPosition, collision2dPosition);
+        Debug.Log("distance = " + distance);
         m_Bell.Play();
-        fingerGun.AddScore(scoreIncrement);
+        if (distance < .1)
+        {
+            fingerGun.AddScore(50);
+        }
+        else if (distance < 0.2)
+        {
+            fingerGun.AddScore(40);
+        }
+        else if (distance < 0.3)
+        {
+            fingerGun.AddScore(30);
+        }
+        else if (distance < 0.4)
+        {
+            fingerGun.AddScore(20);
+        }
+        else
+        {
+            fingerGun.AddScore(10);
+        }
     }
 }
